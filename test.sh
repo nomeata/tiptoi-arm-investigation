6b6633661b15a3cb259c03c379abe9f2275a42fd
Joachim Breitner <mail@joachim-breitner.de>
2020-11-24 17:59:39 +0100

    Improve script

--- test.sh
+++ test.sh
@@ -5,8 +5,10 @@ set -e
 arm-none-eabi-as test.txt -o test.elf
 arm-none-eabi-objcopy -O binary test.elf test.bin
 
-dd status=none iflag=skip_bytes,count_bytes count="$((0x0256130A))" if="WWW Europa.gme" of="WWW Europa2.gme"
-dd status=none if=test.bin conv=notrunc oflag=append of="WWW Europa2.gme"
-dd status=none iflag=skip_bytes,count_bytes skip="$((0x0256130A + 2828))" if="WWW Europa.gme" conv=notrunc oflag=append of="WWW Europa2.gme"
+# diff -u <(xxd OidMain.bin) <(xxd test.bin) || true
 
+dd status=none iflag=skip_bytes,count_bytes count="$((0x0256130A))" if="WWW Europa.gme"                                    of="WWW Europa2.gme"
+dd status=none iflag=skip_bytes,count_bytes if=test.bin count=2828                               conv=notrunc oflag=append of="WWW Europa2.gme"
+dd status=none iflag=skip_bytes,count_bytes if=/dev/zero count=$((2828 - "$(wc -c < test.bin)")) conv=notrunc oflag=append of="WWW Europa2.gme"
+dd status=none iflag=skip_bytes,count_bytes skip="$((0x0256130A + 2828))" if="WWW Europa.gme"    conv=notrunc oflag=append of="WWW Europa2.gme"
 
